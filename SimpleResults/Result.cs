797228e3b1493501d3320ceef160653351e26612
travis@travestyofcode.com <travis@travestyofcode.com>
2025-03-30 08:49:22 -0400

    Adds Result and ResultT classes.

---
+++ SimpleResults/Result.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleResults;
+
+public class Result
+{
+    public int StatusCode { get; set; }
+    public required string StatusMessage { get; set; }
+    public bool WasSuccessful => StatusCode >= 200 && StatusCode <= 299;
+    public List<Error> Errors { get; } = [];
+
+    [SetsRequiredMembers]
+    public Result(int statusCode, string statusMessage)
+    {
+        StatusCode = statusCode;
+        StatusMessage = statusMessage;
+    }
+
+    [SetsRequiredMembers]
+    public Result(int statusCode, string statusMessage, IEnumerable<Error> errors) : this(statusCode, statusMessage)
+    {
+        Errors.AddRange(errors);
+    }
+
+    public static Result Ok() => new(200, "Ok");
+    public static implicit operator Result(Error error) => new(error.ErrorCode, error.ErrorMessage, [error]);
+}
---
+++ SimpleResults/ResultT.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleResults;
+
+public class Result<T> : Result
+{
+    private readonly T? _Value;
+    public T? Value
+    {
+        get => WasSuccessful ? _Value : throw new InvalidOperationException("Unable to access Value of Result when Result has Errors.");
+    }
+
+    [SetsRequiredMembers]
+    public Result(int statusCode, string statusMessage, T? value) : base(statusCode, statusMessage)
+    {
+        _Value = value;
+    }
+
+    [SetsRequiredMembers]
+    public Result(int statusCode, string statusMessage, Error error) : base(statusCode, statusMessage, [error])
+    {
+        _Value = default;
+    }
+
+    public static implicit operator Result<T>(T value) => new(200, "Ok", value);
+    public static implicit operator Result<T>(Error error) => new(error.ErrorCode, error.ErrorMessage, error);
+}
